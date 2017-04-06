1e45576be5680cd892499d7ba859d12b02d384ea
Tymoteusz Cejrowski <tymoteusz.cejrowski@gmail.com>
2017-04-06 13:07:03 +0200

    Added moving sound files to user-folder and creating temperatures.csv files in it.

--- scripts/prepare-parallel-data.sh
+++ scripts/prepare-parallel-data.sh
@@ -11,11 +11,10 @@ if [ -z "$3" ]; then
   echo "You didnt specify catalog name"
   exit
 else
-  #if [ -d "$DATA_RESULT_DIC/$3" ]; then
-  #  rm -rf "$DATA_RESULT_DIC/$3"
-  #fi
-  #mkdir "$DATA_RESULT_DIC/$3"
-  DATA_RESULT_DIC+=/$3
+  if [ -d "$DATA_RESULT_DIC/$3" ]; then
+    rm -rf "$DATA_RESULT_DIC/$3"
+  fi
+  mkdir "$DATA_RESULT_DIC/$3"
 fi
 
 START_FILE_NAME_PATTERN='*'$1'*.csv'
@@ -25,18 +24,34 @@ START_ID="$(find $DATA_ROOT_DIC/mic1 -name $START_FILE_NAME_PATTERN | awk -F'-' 
 END_ID="$(find $DATA_ROOT_DIC/mic1 -name $END_FILE_NAME_PATTERN | awk -F'-' '{print $1}' | awk -F'/' '{print $11}')"
 
 if [ -z "$START_ID" ] || [ -z "$END_ID" ]; then
-    echo "One of id is null. Probably there is no data for one of the timestamps. Check again."
+    printf "One of id is null. Probably there is no data for one of the timestamps. Check again."
     exit
 fi
 NUM_OF_SOUNDS="$(($END_ID-$START_ID+1))";
 
-echo "Your start and end ids are: $START_ID/$END_ID which gives you $NUM_OF_SOUNDS files"
+echo "Your start and end ids are: $START_ID/$END_ID which gives you $NUM_OF_SOUNDS files."
+
+# Create temperature/hum file
+echo "Preparing to downloading temperatures..."
+python3 temperature.py -s $1 -e $2
+echo -e "Done!\n"
+echo "Preparing to downloading humidities..."
+python3 humidity.py -s $1 -e $2
+echo -e "Done!\n"
+
+TEMP_FILE_NAME="$(ls $DATA_ROOT_DIC/thermal/temperature | grep "$1" | grep "$2" | grep ".csv")"
+HUM_FILE_NAME="$(ls $DATA_ROOT_DIC/thermal/humidity | grep "$1" | grep "$2"| grep ".csv")"
+
+echo "Searching $TEMP_FILE_NAME and $HUM_FILE_NAME for proper values..."
 
 for (( c=$START_ID; c<=$END_ID; c++ ))
 do
    FILE="$(find $DATA_ROOT_DIC/mic1 -name '*'$c'*.csv')"
-   DATE="$(find $DATA_ROOT_DIC/mic1 -name '*'$c'*.csv' | awk -F'-' '{print $2 "-" $3 "-" $4}')"
-   DATE=$(echo "$DATE" | tr '[T]' ' ')
-   echo $DATE
-   #cp $FILE $DATA_RESULT_DIC/
+   DATE_T="$(find $DATA_ROOT_DIC/mic1 -name '*'$c'*.csv' | awk -F'-' '{print $2 "-" $3 "-" $4}')"
+   DATE="$(echo "$DATE_T" | tr '[T]' ' ')"
+   ROW="$(cat "$DATA_ROOT_DIC/thermal/temperature/$TEMP_FILE_NAME" | grep "$DATE" | awk -F',' '{print $1}')"
+   echo "$ROW" >> "$DATA_RESULT_DIC/$3/temperatures.csv"
+   cp $FILE "$DATA_RESULT_DIC/$3/$(($c-$START_ID)).csv"
 done
+
+echo "Done! Results are in "$DATA_RESULT_DIC/$3" directory"
