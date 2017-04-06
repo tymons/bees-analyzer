e2217336f7f670f814d50fbe433fa82c1633145a
Tymoteusz Cejrowski <tymoteusz.cejrowski@gmail.com>
2017-04-06 22:36:48 +0200

    Added support for thermal-outdoor.

--- bees.R
+++ bees.R
@@ -7,19 +7,32 @@ dirName <- "full"
 
 dataTempDir <- paste0("~/Projects/003.eUL/workspace/Ranalysis/data/temp/", dirName, "/temperatures.csv")
 dataHumDir <- paste0("~/Projects/003.eUL/workspace/Ranalysis/data/temp/", dirName, "/humidities.csv")
+dataTempOutDir <- paste0("~/Projects/003.eUL/workspace/Ranalysis/data/temp/", dirName, "/temperature-outdoor.csv")
+dataHumOutDir <- paste0("~/Projects/003.eUL/workspace/Ranalysis/data/temp/", dirName, "/humidities-outdoor.csv")
+dataPressOutDir <- paste0("~/Projects/003.eUL/workspace/Ranalysis/data/temp/", dirName, "/pressure-outdoor.csv")
 
-temperaturevalues <- read_csv(dataTempDir, col_names = FALSE)
-humidityvalues <- read_csv(dataHumDir, col_names = FALSE)
-colnames(temperaturevalues) <- "temperature"
-colnames(humidityvalues) <- "humidity"
+temperatureValues <- read_csv(dataTempDir, col_names = FALSE)
+humidityValues <- read_csv(dataHumDir, col_names = FALSE)
+temperatureOutValues <- read_csv(dataTempOutDir, col_names = FALSE)
+humidityOutValues <- read_csv(dataHumOutDir, col_names = FALSE)
+pressureOutValues <- read_csv(dataPressOutDir, col_names = FALSE)
+
+colnames(temperatureValues) <- "Temperature"
+colnames(humidityValues) <- "Humidity"
+colnames(temperatureOutValues) <- "Temperature-outdoor"
+colnames(humidityOutValues) <- "Humidity-outdoor"
+colnames(pressureOutValues) <- "Pressure-outdoor"
 
 soundFeatures = data.frame()
-for(i in 0:(length(temperaturevalues$temperature)-1)) {
+for(i in 0:(length(temperatureValues$Temperature)-1)) {
   soundFeatures = rbind(soundFeatures, analyzeSound(paste0("~/Projects/003.eUL/workspace/Ranalysis/data/temp/", dirName,"/", i, ".csv"), 400))
 }
 
-soundFeatures["Temp"] <- temperaturevalues
-soundFeatures["Hum"] <- humidityvalues
+soundFeatures["Temperature"] <- temperatureValues
+soundFeatures["Humidity"] <- humidityValues
+soundFeatures["Temperature-outdoor"] <- temperatureOutValues
+soundFeatures["Humidity-outdoor"] <- humidityOutValues
+soundFeatures["Pressure-outdoor"] <- pressureOutValues
 
 corrMatrix = matrix(nrow=length(soundFeatures), ncol=length(soundFeatures)) 
 dimnames(corrMatrix) = list(colnames(soundFeatures), colnames(soundFeatures)) 
@@ -30,3 +43,5 @@ for(i in 1:nrow(corrMatrix)) {
   }
 }
 
+corrMatrix <- round(corrMatrix, digits = 2)
+write.csv(corrMatrix, file = "/home/tymons/correlation-matrix.csv")
